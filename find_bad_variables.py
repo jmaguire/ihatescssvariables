18fceddfe12ea936be9df5523bfee9b5210eb134
jmaguire <jmaguire@alumni.stanford.edu>
2024-07-29 14:43:54 -0700

    Cleaning up code

--- find_bad_variables.py
+++ find_bad_variables.py
@@ -2,41 +2,44 @@ import argparse
 import os
 import sys
 import re
-from pathlib import Path
 import json
 import time
+from pathlib import Path
 
 EXCLUDE = ["base", "bourbon", "custom", "neat"]
 
 
-def find_scss_files(directory):
+def collect_scss_files_in_directory(directory):
+    """
+    Walk through all directories and files in the provided directory,
+    filtering out directories listed in EXCLUDE, and collect all .scss files.
+    """
+    directory_path = Path(directory)
     scss_files = []
+
     # Walk through all directories and files in the provided directory
-    for root, dirs, files in os.walk(directory):
-        dirs[:] = [d for d in dirs if d not in EXCLUDE]
-        # Filter and add .scss files to the list
-        for file in files:
-            if file.endswith(".scss"):
-                scss_files.append(os.path.join(root, file))
+    for path in directory_path.rglob('*.scss'):
+        if path.parent.name not in EXCLUDE:
+            scss_files.append(str(path))
+
     return scss_files
 
-## Gets variables where there are used like var(--test-color);
 def extract_css_variables(filename):
+    """
+    Extract all CSS variable usages formatted as var(--variable-name) from a given file.
+    """
     variable_pattern = r'var\((--[a-zA-Z0-9-]+)\)'
     variable_names = set()  # Use a set to avoid duplicate variable names
 
     with open(filename, 'r') as f:
         file_contents = f.read()
 
-    # Find all matches for the pattern
-    matches = re.findall(variable_pattern, file_contents)
-    for match in matches:
-        variable_names.add(match)
+    return set(re.findall(variable_pattern, file_contents))
 
-    return variable_names
-
-## Gets variables where there are declared like --test-color:
 def extract_css_variable_declarations(filename):
+    """
+    Extract all CSS variable declarations formatted as --variable-name: from a given file.
+    """
     variable_pattern = r'(--[\w-]+):'
     variable_names = set()  # Use a set to avoid duplicate variable names
 
@@ -47,6 +50,10 @@ def extract_css_variable_declarations(filename):
     return set(re.findall(variable_pattern, file_contents))
 
 def get_undeclared_css_variables(file_list, declared_variables):
+    """
+    Create a dictionary of undeclared variables with the value cooresponding to the files
+    where the variable is used.
+    """
     undeclared_variables_combined = {}
     for filename in file_list:
         filename_only = Path(filename).name
@@ -57,6 +64,17 @@ def get_undeclared_css_variables(file_list, declared_variables):
                 undeclared_variables_combined[variable] = []
             undeclared_variables_combined[variable].append(filename_only)
     return undeclared_variables_combined
+
+def get_unused_css_variables(file_list, declared_variables):
+    """
+    Create a list of unused variables.
+    """
+    # set of variables that are never used
+    used_variables_combined = set()
+    for filename in file_list:
+        filename_only = Path(filename).name
+        used_variables_combined = used_variables_combined.union(extract_css_variables(filename))
+    return list(declared_variables - used_variables_combined)
 
 
 def main():
@@ -80,18 +98,23 @@ def main():
     # Get scss files
     scss_files = []
     for path in args.directory:
-        scss_files.extend(find_scss_files(path))
+        scss_files.extend(collect_scss_files_in_directory(path))
     print(f'Found {len(scss_files)} scss files')
 
 
-    data = get_undeclared_css_variables(scss_files, css_variables)
-    files = {file for var, files in data.items() for file in files}
+    undeclared = get_undeclared_css_variables(scss_files, css_variables)
+    files = {file for var, files in undeclared.items() for file in files}
     
-    print(f'Found {len(data)} undeclared variables')
+    print(f'Found {len(undeclared)} undeclared variables')
     print(f'Found across {len(files)} files')
     
     with open("undeclared_variables.json", "w") as f:
-        json.dump(data, f, indent=4)
+        json.dump(undeclared, f, indent=4)
+    
+    unused = get_unused_css_variables(scss_files, css_variables)
+    print(f'Found {len(unused)} unused variables')
+    with open("unused_variables.json", "w") as f:
+        json.dump(unused, f, indent=4)
 
 
 if __name__ == "__main__":
